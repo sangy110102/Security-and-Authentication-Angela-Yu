a9d0b2effaffae62badbeb67cb3002c2b94da106
Sangeeta Sahoo <sangy110102@gmail.com>
2022-07-22 00:51:08 +0530

    Level 4 - Salting & hashing passwords using bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
 //const encrypt = require('mongoose-encryption');
-const md5 = require('md5');
+//const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -46,16 +48,22 @@ app.route('/login')
 .post( (req,res) => {
     User.findOne({email : req.body.username},(err,foundUser) => {
         if(!err){
-            if(foundUser.password === md5(req.body.password)){
-                res.render('secrets');
+            if(foundUser){
+                bcrypt.compare(req.body.password,foundUser.password,(err,result) =>{
+                    if(result){
+                        res.render('secrets');
+                    }else{
+                        res.send('Incorrect Password... Try Again...');
+                    }
+               });
             }else{
-                res.send('Incorrect Password... Try Again...');
+                res.send('User not found...');
             }
         }else{
             res.send(err);
         }
     });
-})
+});
 
 
 app.route('/register')
@@ -63,17 +71,20 @@ app.route('/register')
     res.render('register')
 })
 .post((req,res) => {
-    const newUser = new User({
-        email : req.body.username,
-        password : md5(req.body.password)
-      });
-    newUser.save((err) => {
-        if(!err){
-            res.render('login');
-        }else{
-            res.send(err);
-        }
+    bcrypt.hash(req.body.password,saltRounds,(err,hash) => {
+        const newUser = new User({
+            email : req.body.username,
+            password : hash
+          });
+        newUser.save((err) => {
+            if(!err){
+                res.render('login');
+            }else{
+                res.send(err);
+            }
+        });
     });
+    
 });
 
 
