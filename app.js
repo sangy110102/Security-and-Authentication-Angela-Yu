ef96a08f24a65b01bce517af2f7fa404143a33b0
Sangeeta Sahoo <sangy110102@gmail.com>
2022-07-21 20:02:43 +0530

    Level 2 encryption

---
+++ app.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+const uri =
+  "mongodb://localhost:27017/userDB";
+
+mongoose.connect(uri,{useNewUrlParser : true});
+
+const userSchema = new mongoose.Schema({
+    email : String,
+    password : String
+});
+
+const secret = 'Thisisourlittlesecret.';
+
+userSchema.plugin(encrypt,{secret : secret, encryptedFields : ['password'] }); 
+
+const User = mongoose.model('User',userSchema);
+
+const user1 = new User({
+    email : '',
+    password : ''
+  });
+
+app.get('/',(req,res) => {
+    res.render('home')
+});
+
+app.route('/login')
+.get( (req,res) => {
+    res.render('login')
+})
+.post( (req,res) => {
+    User.findOne({email : req.body.username},(err,foundUser) => {
+        if(!err){
+            if(foundUser.password === req.body.password){
+                res.render('secrets');
+            }else{
+                res.send('Incorrect Password... Try Again...');
+            }
+        }else{
+            res.send(err);
+        }
+    });
+})
+
+
+app.route('/register')
+.get((req,res) => {
+    res.render('register')
+})
+.post((req,res) => {
+    const newUser = new User({
+        email : req.body.username,
+        password : req.body.password
+      });
+    newUser.save((err) => {
+        if(!err){
+            res.render('login');
+        }else{
+            res.send(err);
+        }
+    });
+});
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
